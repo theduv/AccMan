e64c3b8697f2711b723b96ede33da8ca48d8d625
theduv <tduval@student.42.com>
2021-04-10 21:06:46 +0200

    had to add the node_modules folder in order to build the app on netlify

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -57,7 +57,7 @@ const Home = () => {
         </button>
       </div>
       {addingPlatform && (
-        <form onSubmit={onSubmitForm}>
+        <form onSubmit={onSubmitForm} id="formAddPlatform">
           <input
             className="addInput"
             type="text"
