7c2b0995a25bcef211aa1de839b40f17406a1f98
theduv <tduval@student.42.com>
2021-04-11 00:09:46 +0200

    upgraded the footer, fixed some overflow issues on Home

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
 import { FiGithub } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
 import "../Styles/Footer.css";
 
 const Footer = () => {
@@ -10,6 +11,13 @@ const Footer = () => {
         rel="noopener noreferrer"
       >
         <FiGithub id="githubIcon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/theduv/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FiLinkedin id="linkedinIcon" />
       </a>
     </div>
   );
